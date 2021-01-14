3db1d4351d93a0f15b2c384aae9ceeb0f7a23524
jimjguo <jimjguo@tencent.com>
2021-01-14 10:43:30 +0800

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by jimjguo on 2021/1/14.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
